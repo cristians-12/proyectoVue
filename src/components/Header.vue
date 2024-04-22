d395fc15722d628f9b38282b86e12890ee1257ad
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-04-22 17:45:43 -0500

    popup Fixed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,26 @@ import facebook from "../assets/img/img1.png";
 import insta from "../assets/img/img2.png";
 import yt from "../assets/img/img3.png";
 
+import { useMedia } from "../composables/media";
+
 import popUpMobile from "./popUpMobile.vue";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
 const hide = ref(false);
 
-const toggle = ()=>{
+const toggle = () => {
   hide.value = !hide.value;
-}
+};
+
+const isSmall = useMedia("(max-width:700px)");
+
+onMounted(() => {
+  const handleResize = () => {
+    hide.value = isSmall.value;
+  };
+  handleResize();
+  window.addEventListener("resize", handleResize);
+});
 </script>
 
 <template>
@@ -42,7 +54,12 @@ const toggle = ()=>{
       </div>
     </div>
     <figure class="md:hidden text-white block">
-      <svg @click="toggle" class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <svg
+        @click="toggle"
+        class="w-5"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
         <path
           fill="#ffffff"
           d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
