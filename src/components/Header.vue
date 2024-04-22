3a7d72099a43a4629b0f587edc0e0b297396cc33
Eggward12 <cristian.sanchez.casd@gmail.com>
2024-04-22 17:33:23 -0500

    Popup Added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,15 @@ import image from "../assets/img/logo.png";
 import facebook from "../assets/img/img1.png";
 import insta from "../assets/img/img2.png";
 import yt from "../assets/img/img3.png";
+
+import popUpMobile from "./popUpMobile.vue";
+import { ref } from "vue";
+
+const hide = ref(false);
+
+const toggle = ()=>{
+  hide.value = !hide.value;
+}
 </script>
 
 <template>
@@ -33,7 +42,7 @@ import yt from "../assets/img/img3.png";
       </div>
     </div>
     <figure class="md:hidden text-white block">
-      <svg class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <svg @click="toggle" class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path
           fill="#ffffff"
           d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
@@ -41,4 +50,5 @@ import yt from "../assets/img/img3.png";
       </svg>
     </figure>
   </header>
+  <popUpMobile v-show="hide" />
 </template>
--- src/components/popUpMobile.vue
+++ src/components/popUpMobile.vue
@@ -1,6 +1,36 @@
 <script setup>
-    
+import image from "../assets/img/logo.png";
+import facebook from "../assets/img/img1.png";
+import insta from "../assets/img/img2.png";
+import yt from "../assets/img/img3.png";
 </script>
 <template>
-    
+  <div
+    class="fixed top-16 sm:top-20 right-0 z-20 w-44 pt-5 px-3 pb-3 bg-[black] rounded-lg text-white font-bold"
+  >
+    <div class="flex items-center gap-5">
+      <figure
+        class="rounded-full border w-fit px-2 py-1 border-[#84754E] md:px-2 md:py-1 lg:px-3 lg:py-2"
+      >
+        <img :src="facebook" alt="facebook logo" />
+      </figure>
+      <figcaption>Facebook</figcaption>
+    </div>
+    <div class="flex items-center gap-5 mt-2">
+      <figure
+        class="rounded-full border w-fit px-1 py-1 border-[#84754E] md:px-2 md:py-1 lg:px-3 lg:py-2"
+      >
+        <img :src="insta" alt="instagram logo" />
+      </figure>
+      <figcaption>Instagram</figcaption>
+    </div>
+    <div class="flex items-center gap-5 mt-2">
+      <figure
+        class="rounded-full border w-fit px-1 py-1 border-[#84754E] md:px-2 md:py-1 lg:px-3 lg:py-2"
+      >
+        <img :src="yt" alt="Youtube logo" />
+      </figure>
+      <figcaption>YouTube</figcaption>
+    </div>
+  </div>
 </template>
